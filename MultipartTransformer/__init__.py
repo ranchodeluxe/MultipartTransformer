9c96a03e2a5e9808687a018d1986e6ac8931532f
thebigspoon <gregcorradini@gmail.com>
2013-09-20 10:13:55 -0700

    Trying to create one single API across 1.8x to 2.0 versions

--- MultipartTransformer/__init__.py
+++ MultipartTransformer/__init__.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 def classFactory(iface):
-    # load MultipartTransformer class from file MultipartTransformer
-    from multiparttransformer import MultipartTransformer
-    return MultipartTransformer(iface)
+    import sip
+    sip_qvariant = 1
+    try:
+        sip_qvariant = sip.getapi("QVariant")
+    except Exception:
+        pass
+
+    if sip_qvariant > 1:
+        # Use the new API style
+        # load MultipartTransformerv20 class from file MultipartTransformer_v20
+        from multiparttransformer_v20 import MultipartTransformerv20
+        return MultipartTransformerv20(iface)
+    else:
+        # Use the old API style
+        # load MultipartTransformer class from file MultipartTransformer
+        from multiparttransformer import MultipartTransformer
+        return MultipartTransformer(iface)
